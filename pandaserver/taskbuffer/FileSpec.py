b4c5c05d2b56bc907be7ddcb1f0815bd0d13c203
ruslan33 <rmashinistov@gmail.com>
2016-11-02 13:50:37 +0300

    Back to the sequence for MySQL

--- pandaserver/taskbuffer/FileSpec.py
+++ pandaserver/taskbuffer/FileSpec.py
@@ -171,12 +171,14 @@ class FileSpec(object):
         ret = "VALUES("
         for attr in cls._attributes:
             if useSeq and cls._seqAttrMap.has_key(attr):
-                if panda_config.backend == 'mysql':
-                    # mysql
-                    ret += "%s," % "NULL"
-                else:
-                    # oracle
-                    ret += "%s," % cls._seqAttrMap[attr]
+                #if panda_config.backend == 'mysql':
+                #    # mysql
+                #    ret += "%s," % "NULL"
+                #else:
+                #    # oracle
+                #    ret += "%s," % cls._seqAttrMap[attr]
+                #Back to the sequence for MySQL
+                ret += "%s," % cls._seqAttrMap[attr]
             else:
                 ret += ":%s," % attr
         ret = ret[:-1]
